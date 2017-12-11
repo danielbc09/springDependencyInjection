92927a3b4066462f8d827feddd2c193cbf591eef
Jairo Daniel Bautista <bautisj@colpatria.com>
2017-12-11 14:53:54 -0500

    Controller modified

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,7 +1,15 @@
 package guru.springframework.controllers;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by bautisj on 12/11/2017.
  */
+@Controller
 public class MyController {
+
+    public String hello(){
+        System.out.println("Hello My Controllelr");
+        return "foo";
+    }
 }
