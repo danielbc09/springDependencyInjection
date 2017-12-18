965b6c4f600aee79719433d4169507aa7e3183ff
Jairo Daniel Bautista <bautisj@colpatria.com>
2017-12-18 16:12:12 -0500

    Multiple Property Files

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
@@ -13,7 +15,10 @@ import org.springframework.core.env.Environment;
  * Created by jt on 6/7/17.
  */
 @Configuration
-@PropertySource("classpath:datasource.properties")
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
     @Autowired
@@ -26,6 +31,13 @@ public class PropertyConfig {
     @Value("${guru.dburl}")
     String dburl;
 
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+    @Value("${guru.jms.url}")
+    String jmsUrl;
+
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -34,6 +46,15 @@ public class PropertyConfig {
         fakeDataSource.setUrl(dburl);
 
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker jmsBroker = new FakeJmsBroker();
+        jmsBroker.setUsername(jmsUsername);
+        jmsBroker.setPassword(jmsPassword);
+        jmsBroker.setUrl(jmsUrl);
+        return jmsBroker;
     }
 
     @Bean
